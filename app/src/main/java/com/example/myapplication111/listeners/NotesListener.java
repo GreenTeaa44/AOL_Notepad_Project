f37a1bb34ab1b2603074b8a9ca958184cde5b957
Handaya <handaya.riawan@binus.ac.id>
2023-12-28 20:29:52 +0700

    First Commit

---
+++ app/src/main/java/com/example/myapplication111/listeners/NotesListener.java
@@ -0,0 +1,7 @@
+package com.example.myapplication111.listeners;
+
+import com.example.myapplication111.entities.Note;
+
+public interface NotesListener {
+    void onNoteClicked(Note note, int position);
+}
